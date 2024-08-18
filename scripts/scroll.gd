af5ac5293225b06bc1fae5402fe229ca4bd09d52
SweetCaroline36 <melodycharm6@gmail.com>
2024-08-18 10:41:44 -0500

    Scroll mask is complete
    still a bit janky, but will serve for now. also added papyrus font

--- scripts/scroll.gd
+++ scripts/scroll.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @export var animator : AnimationPlayer
+@export var text_box : RichTextLabel
+
+var scroll_text = "Hello world\nwoot woot\nthis is a test to see if wrapping works"
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	text_box.text = scroll_text
 	animator.play("open_scroll")
